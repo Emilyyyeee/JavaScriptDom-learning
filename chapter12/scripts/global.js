ae49df2bda01e925b755f670ee11fe7b83996128
konyahoshi <konyahoshi@qq.com>
2017-03-15 19:43:21 +0800

    chapter12

---
+++ chapter12/scripts/global.js
@@ -0,0 +1,60 @@
+function addLoadEvent(func) 
+{
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') 
+  {
+    window.onload = func;
+  } 
+  else 
+  {
+    window.onload = function() 
+    {
+	    oldonload();
+	    func();
+    }
+  }
+}
+	
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+function addClass(element,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName+= " ";
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
+function highlightPage()
+{
+	if(!document.getElementById()) return false;
+	if(!document.getElementsByTagName()) return false;
+	var headers = document.getElementsByTagName("header");
+	if(headers.length == 0)return false;
+	var navs = headers[0].getElementsByTagName('nav');
+  	if (navs.length == 0) return false;
+  	var links = navs[0].getElementsByTagName("a");
+  	for (var i=0; i<links.length; i++) 
+  	{
+		var linkurl;
+		for (var i=0; i<links.length; i++) 
+		{
+			linkurl = links[i].getAttribute("href");
+			if (window.location.href.indexOf(linkurl) != -1) 
+			{
+				links[i].className = "here";
+				var linktext = links[i].lastChild.nodeValue.toLowerCase();
+				document.body.setAttribute("id",linktext);
+		    }
+	  	}
+  	}	
+}
